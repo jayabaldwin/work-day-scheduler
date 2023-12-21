7bbcff0f0a791b232f45006c895942c9fc6741df
Jaya <148409249+jayabaldwin@users.noreply.github.com>
2023-12-21 15:18:42 +1100

    removed storage functions, not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,22 +3,23 @@
 $(document).ready(function () {
 
 
-  // added event listener for the saved button
+  // event listener for click events on the saved button
   $('.saveBtn').on('click', function(){
+    preventDefault();
     var blockText = $(this).siblings('.description').val();
     var blockTime = $(this).parent().attr('id');
     console.log(blockText, blockTime);
-    // stores data of hour-id and corresponding text
+    // stores data of hour-id and corresponding text as key-value pairs
     localStorage.setItem(blockTime, blockText);
   });
 
-  
 
   // set current hour using dayjs
   var currentHour = dayjs().hour()
   console.log("Hour", currentHour);
 
   console.log("Timeblock", $('.time-block'));
+  // iterate over all time blocks using each() ehich returns an object
   $('.time-block').each(function(){
     // separate the numerical value at the end of the id i.e. "hour-10" and parse it as an integer
     var blockHour = parseInt($(this).attr('id').split('-')[1]);
@@ -37,23 +38,41 @@ $(document).ready(function () {
       $(this).addClass('past');
       console.log("In the past");
     }
+
+
   });
 
 
-  
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   
-  // grab an element you want to refer to by id and class, list all elements and give it a value with .val method
+  // grab element by id and class, list all elements and give a value with .val method
   // check in local storage if the key pairs exist localStorage.getItem and give value to text area
   // $("#hour-5 .description")
 
+  // localStorage.getItem(blockText);
+
+  // $(window).on('load', function(){
+  //   var savedData = localStorage.getItem(blockText)
 
 
 
+  // });
 
-  // Added code to display the current date and attached to header through id
+  // select each div element by ID and class
+  // $("#hour-9 .description").val();
+  // $("#hour-10 .description").val();
+  // $("#hour-11 .description").val();
+  // $("#hour-12 .description").val();
+  // $("#hour-13 .description").val();
+  // $("#hour-14 .description").val();
+  // $("#hour-15 .description").val();
+  // $("#hour-16 .description").val();
+  // $("#hour-17 .description").val();
+
+
+  // Displays the current date in header through ID
   const currentDate = dayjs();
   const formattedDate = currentDate.format('MMMM D, YYYY');
   $('#currentDay').text(formattedDate);
